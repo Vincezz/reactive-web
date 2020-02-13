06a0c1b613337c5cd2a84c893e45b80a8ea408f6
vince <vincezz59@gmail.com>
2020-02-13 22:27:14 +0100

    CHORE: add some endpoint

--- reactive-web/src/main/java/com/vdy/reactive/reactiveweb/controller/UserController.java
+++ reactive-web/src/main/java/com/vdy/reactive/reactiveweb/controller/UserController.java
@@ -2,6 +2,7 @@ package com.vdy.reactive.reactiveweb.controller;
 
 import com.vdy.reactive.reactiveweb.model.User;
 import com.vdy.reactive.reactiveweb.service.UserService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,18 @@ public class UserController {
     @ResponseStatus(HttpStatus.CREATED)
     private Mono<User> createUser(@RequestBody User user) {
         return userService.createUser(user);
+    }
+
+    @GetMapping("/generate")
+    @ResponseStatus(HttpStatus.CREATED)
+    private Mono<User> generate() {
+        return userService.createUser(User.builder().firstName("generated").lastName("user").build());
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    private Mono<User> getUser(@PathVariable Long id) {
+        return userService.getUser(id);
     }
 
 }
--- reactive-web/src/main/java/com/vdy/reactive/reactiveweb/service/UserService.java
+++ reactive-web/src/main/java/com/vdy/reactive/reactiveweb/service/UserService.java
@@ -3,18 +3,25 @@ package com.vdy.reactive.reactiveweb.service;
 import com.vdy.reactive.reactiveweb.model.User;
 import com.vdy.reactive.reactiveweb.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
 
     @Transactional
     public Mono<User> createUser(User user) {
+        log.info("Create user {}", user.toString());
         return userRepository.save(user);
+    }
+
+    public Mono<User> getUser(Long id) {
+        return userRepository.findById(id);
     }
 }
